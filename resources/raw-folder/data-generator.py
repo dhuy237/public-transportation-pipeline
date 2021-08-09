62e5a31515d561fbbc3e3c5146721fc61bd393c7
d.huy237 <d.huy723@gmail.com>
2021-08-10 00:13:30 +0700

    Add all table data generator code

--- resources/raw-folder/data-generator.py
+++ resources/raw-folder/data-generator.py
@@ -7,7 +7,6 @@ from faker import Faker
 import pandas as pd
 import seaborn
 
-
 def create_bus_stop(output_path, numOfRecords=100, seed_num=0):
     fake = Faker()
     Faker.seed(seed_num)
@@ -31,39 +30,6 @@ def create_bus_stop(output_path, numOfRecords=100, seed_num=0):
                     'district': fake.random_int(1, 12),
                     'latitude': fake_location_data[0],
                     'longtitude': fake_location_data[1]
-                }
-            )
-    return filename
-
-def create_route(output_path, numOfRecords=20, seed_num=0):
-    fake = Faker()
-    Faker.seed(seed_num)
-
-    filename = output_path+'/route.csv'
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['route_id', 'agency_id', 'route_name', 'departure_name', 'destination_name', 
-                      'duration', 'fare', 'frequency', 'number_of_stop', 'number_of_bus', 'operating_hour', 'distance']
-
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for i in range(numOfRecords):
-            departure_name = fake.street_name()
-            destination_name = fake.street_name()
-
-            writer.writerow(
-                {
-                    'route_id': i + 1,
-                    'agency_id': fake.random_int(100, 100 + numOfRecords),
-                    'route_name': departure_name + " - " + destination_name,
-                    'departure_name': departure_name,
-                    'destination_name': destination_name,
-                    'duration': fake.random_int(100, 120),
-                    'fare': fake.random_int(5, 10),
-                    'frequency': fake.random_int(15, 20),
-                    'number_of_stop': fake.random_int(10, 20),
-                    'operating_hour': fake.random_int(12, 16),
-                    'distance': fake.random_int(1, 15)
                 }
             )
     return filename
@@ -106,6 +72,7 @@ def create_bus(output_path, numOfRecords=60, seed_num=0, numOfRouteID=20):
         'normal': 40,
         'small': 30
     }
+    route_dict = {x:0 for x in range(1, numOfRouteID + 1)}
 
     with open(filename, 'w', newline='') as csvfile:
         fieldnames = ['bus_code', 'route_id', 'number_of_seat', 'type']
@@ -114,13 +81,116 @@ def create_bus(output_path, numOfRecords=60, seed_num=0, numOfRouteID=20):
         writer.writeheader()
 
         for i in range(numOfRecords):
+            route_random_value = fake.random_int(1, numOfRouteID)
+            
+            route_dict[route_random_value] += 1
+
             bus_type, bus_seat = random.choice(list(bus_dict.items()))
             writer.writerow(
                 {
                     'bus_code': 100 + i,
-                    'route_id': fake.random_int(1, numOfRouteID),
+                    'route_id': route_random_value,
                     'number_of_seat': bus_seat,
                     'type': bus_type
+                }
+            )
+    return filename, route_dict
+
+def create_route(output_path, route_dict, numOfRecords=20, seed_num=0):
+    fake = Faker()
+    Faker.seed(seed_num)
+
+    filename = output_path+'/route.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['route_id', 'agency_id', 'route_name', 'departure_name', 'destination_name', 
+                      'duration', 'fare', 'frequency', 'number_of_stop', 'number_of_bus', 'operating_hour', 'distance']
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in range(numOfRecords):
+            departure_name = fake.street_name()
+            destination_name = fake.street_name()
+
+            writer.writerow(
+                {
+                    'route_id': i + 1,
+                    'agency_id': fake.random_int(100, 100 + numOfRecords),
+                    'route_name': departure_name + " - " + destination_name,
+                    'departure_name': departure_name,
+                    'destination_name': destination_name,
+                    'duration': fake.random_int(100, 120),
+                    'fare': fake.random_int(5, 10),
+                    'frequency': fake.random_int(15, 20),
+                    'number_of_stop': fake.random_int(10, 20),
+                    'number_of_bus': route_dict[i + 1],
+                    'operating_hour': fake.random_int(12, 16),
+                    'distance': fake.random_int(1, 15)
+                }
+            )
+    return filename
+
+def create_stop_time(output_path, numOfRecords=10000, seed_num=0, numOfBusStop=100):
+    fake = Faker()
+    Faker.seed(seed_num)
+
+    filename = output_path+'/stop_time.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['stop_time_id', 'trip_id', 'bus_stop_id']
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in range(numOfRecords):
+            writer.writerow(
+                {
+                    'stop_time_id': numOfRecords + i,
+                    'trip_id': i,
+                    'bus_stop_id': fake.random_int(1000, 1000 + numOfBusStop)
+                }
+            )
+    return filename
+
+def create_trip(output_path, numOfRecords=10000, seed_num=0, numOfBus=60):
+    fake = Faker()
+    Faker.seed(seed_num)
+
+    filename = output_path+'/trip.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['trip_id', 'bus_code', 'trip_headsign', 'timestamp_stop', 'number_of_ticket']
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in range(numOfRecords):
+            writer.writerow(
+                {
+                    'trip_id': i,
+                    'bus_code': fake.random_int(100, 100 + numOfBus),
+                    'trip_headsign': fake.company(),
+                    'timestamp_stop': fake.date_time_this_year().replace(tzinfo=timezone.utc).timestamp(),
+                    'number_of_ticket': fake.random_int(1, 30)
+                }
+            )
+    return filename
+
+def create_stop_route(output_path, numOfRecords=100, seed_num=0, numOfBus=60):
+    fake = Faker()
+    Faker.seed(seed_num)
+
+    filename = output_path+'/stop_route.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['bus_stop_id', 'route_id', 'arrival_time']
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in range(numOfRecords):
+            writer.writerow(
+                {
+                    'bus_stop_id': 1000 + i,
+                    'route_id': i,
+                    'arrival_time': fake.time()
                 }
             )
     return filename
