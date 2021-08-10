1d71d88b6fcf3ea78e352a7f6b8b627f8388f793
d.huy237 <d.huy723@gmail.com>
2021-08-10 10:29:23 +0700

    Add setup database

--- src/mssql/init_mssql.sql
+++ src/mssql/init_mssql.sql
@@ -1,4 +1,20 @@
 -- Set up Database
+USE master ;  
+GO  
+CREATE DATABASE PublicTransportation  
+ON   
+( NAME = Sales_dat,  
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\saledat.mdf',  
+    SIZE = 10,  
+    MAXSIZE = 50,  
+    FILEGROWTH = 5 )  
+LOG ON  
+( NAME = Sales_log,  
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\salelog.ldf',  
+    SIZE = 5MB,  
+    MAXSIZE = 25MB,  
+    FILEGROWTH = 5MB ) ;  
+GO  
 
 -- set up Schema
 
