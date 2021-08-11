be19c9a0773b6547ff46009975c0408270282e93
d.huy237 <d.huy723@gmail.com>
2021-08-11 21:19:59 +0700

    Add create database on snowflake

--- src/mssql/init_mssql.sql
+++ src/mssql/init_mssql.sql
@@ -3,13 +3,13 @@ USE master ;
 GO  
 CREATE DATABASE PublicTransportation  
 ON   
-( NAME = Sales_dat,  
+( NAME = PT_dat,  
     FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ptdat.mdf',  
     SIZE = 10,  
     MAXSIZE = 50,  
     FILEGROWTH = 5 )  
 LOG ON  
-( NAME = Sales_log,  
+( NAME = PT_log,  
     FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\ptlog.ldf',  
     SIZE = 5MB,  
     MAXSIZE = 25MB,  
