f67d9b6fa66a07270534dabb387338754db1ca7d
huynguyen314 <billnguyen2091@gmail.com>
2021-09-08 17:15:09 +0700

    Add create_proxy_and_set_job.sql file

---
+++ src/mssql/create_proxy_and_set_job.sql
@@ -0,0 +1,182 @@
+--- Create Proxy To Run Jobs
+USE [msdb]
+GO
+
+EXEC msdb.dbo.sp_add_proxy @proxy_name=N'JobAgentProxy',@credential_name=N'DBAdminCredential', 
+		@enabled=1
+GO
+
+EXEC msdb.dbo.sp_grant_proxy_to_subsystem @proxy_name=N'JobAgentProxy', @subsystem_id=11
+GO
+
+--- Create Operators To Send Mail
+USE [msdb]
+GO
+
+EXEC msdb.dbo.sp_add_operator @name=N'DBAdmin', 
+		@enabled=1, 
+		@weekday_pager_start_time=80000, 
+		@weekday_pager_end_time=180000, 
+		@saturday_pager_start_time=80000, 
+		@saturday_pager_end_time=180000, 
+		@sunday_pager_start_time=80000, 
+		@sunday_pager_end_time=180000, 
+		@pager_days=127, 
+		@email_address=N'testingdata2021@outlook.com', 
+		@category_name=N'[Uncategorized]'
+GO
+
+--- Create Alerts
+USE [msdb]
+GO
+
+EXEC msdb.dbo.sp_add_alert @name=N'LoadDataNotify', 
+		@message_id=0, 
+		@severity=1, 
+		@enabled=1, 
+		@delay_between_responses=300, 
+		@include_event_description_in=1, 
+		@database_name=N'PublicTransportation', 
+		@category_name=N'[Uncategorized]', 
+		@job_id=N'43d76524-8029-443b-baba-1fd61d3291f7'
+GO
+
+
+--- Create Job: LoadDataServerAndSnowflake
+USE [msdb]
+GO
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'LoadDataServerAndSnowflake', 
+		@enabled=1, 
+		@notify_level_eventlog=2, 
+		@notify_level_email=3, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'DBAdmin', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Server_Snowflake]    *******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Server_Snowflake', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'SSIS', 
+		@command=N'/ISSERVER "\"\SSISDB\PublicTransportation\project1_team7_FA\LoadRawDataToServerAndSnowflake.dtsx\"" /SERVER CVP00174163A /ENVREFERENCE 5 /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E', 
+		@database_name=N'master', 
+		@flags=0, 
+		@proxy_name=N'JobAgentProxy'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'LoadDataServer_Snowflake', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20210908, 
+		@active_end_date=99991231, 
+		@active_start_time=230000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'f3187d87-7e8b-455f-a511-b49df7d08061'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+--- Create Job: UnloadDataFromSnowflake
+USE [msdb]
+GO
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'UnloadDataFromSnowflake', 
+		@enabled=1, 
+		@notify_level_eventlog=2, 
+		@notify_level_email=3, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'DBAdmin', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [UnloadFromSnowflake]    ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'UnloadFromSnowflake', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'SSIS', 
+		@command=N'/ISSERVER "\"\SSISDB\PublicTransportation\project1_team7_FA\UnloadDataFromSnowflake.dtsx\"" /SERVER CVP00174163A /ENVREFERENCE 5 /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E', 
+		@database_name=N'master', 
+		@flags=0, 
+		@proxy_name=N'JobAgentProxy'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'UnloadDataSnowflake', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20210908, 
+		@active_end_date=99991231, 
+		@active_start_time=233000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'c26bac6e-8b8b-48f7-97c9-7e1178c0efb9'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
--- src/mssql/init_mssql.sql
+++ src/mssql/init_mssql.sql
@@ -1,189 +1,350 @@
--- Set up Database
-USE master;  
-GO  
-CREATE DATABASE PublicTransportation  
-ON  
-GO  
-
--- Set up Schema
+--Create database
+CREATE DATABASE [PublicTransportation];
+GO
 USE [PublicTransportation];
 GO
-CREATE SCHEMA Agency;
-GO
-CREATE SCHEMA Schedule;
+CREATE SCHEMA [Bus];
 GO
 
--- Create Table
+--Create tables for schema Bus
 
--- Agency.Agency
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Agency].[Agency](
-	[agency_id] [int] NOT NULL,
-	[name] [nvarchar](50) NOT NULL,
-	[phone_number] [nvarchar](25) NOT NULL,
-	[address] [nvarchar](200) NOT NULL,
-	[operating_hour_start] [time](7) NOT NULL,
-	[operating_hour_end] [time](7) NOT NULL,
- CONSTRAINT [PK_Agency] PRIMARY KEY CLUSTERED 
+CREATE TABLE [Bus].[BusType]
 (
-	[agency_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+      [bus_type_id] INT NOT NULL,
+	  [bus_type] VARCHAR(50) NOT NULL,
+	  [fare] FLOAT NOT NULL,
+	  [fare_status] VARCHAR(50),
+	  [modified_date] DATETIME,
+CONSTRAINT PK_bustype PRIMARY KEY([bus_type_id]) 
+);
 
--- Agency.Bus
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Agency].[Bus](
-	[bus_code] [int] NOT NULL,
-	[route_id] [int] NOT NULL,
-	[number_of_seat] [int] NOT NULL,
-	[type] [nvarchar](10) NOT NULL,
- CONSTRAINT [PK_Bus] PRIMARY KEY CLUSTERED 
+CREATE TABLE [Bus].[BusRoute]
 (
-	[bus_code] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+      [route_id] VARCHAR(50) NOT NULL,
+	  [route_name] VARCHAR(50) NOT NULL,
+	  [bus_type_id] INT NOT NULL,
+	  [depart_address] VARCHAR(100) NOT NULL,
+	  [number_of_busstop] INT NOT NULL,
+	  [standard_duration] INT NOT NULL,
+	  [frequency] INT NOT NULL,
+	  [route_distance] FLOAT NOT NULL,
+	  [operating_start_hour] TIME NOT NULL,
+	  [operating_end_hour] TIME NOT NULL,
+	  [modified_date] DATETIME,
+CONSTRAINT PK_busroute PRIMARY KEY ([route_id])
+);
 
--- Schedule.BusStop
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Schedule].[BusStop](
-	[bus_stop_id] [int] NOT NULL,
-	[name] [nvarchar](100) NOT NULL,
-	[street] [nvarchar](100) NOT NULL,
-	[district] [int] NOT NULL,
-	[latitude] [float] NOT NULL,
-	[longtitude] [float] NOT NULL,
- CONSTRAINT [PK_BusStop] PRIMARY KEY CLUSTERED 
+CREATE TABLE [Bus].[BusInfo]
 (
-	[bus_stop_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+      [bus_code] VARCHAR(50) NOT NULL,
+	  [route_id] VARCHAR(50) NOT NULL,
+	  [route_id_status] VARCHAR(50) NOT NULL,
+	  [seat_capacity] INT NOT NULL,
+	  [max_capacity] INT NOT NULL,
+	  [modified_date] DATETIME,
+CONSTRAINT PK_businfo PRIMARY KEY ([bus_code])
+);
 
--- Schedule.Route
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Schedule].[Route](
-	[route_id] [int] NOT NULL,
-	[agency_id] [int] NOT NULL,
-	[route_name] [nvarchar](50) NOT NULL,
-	[departure_name] [nvarchar](50) NOT NULL,
-	[destination_name] [nvarchar](50) NOT NULL,
-	[duration] [int] NOT NULL,
-	[fare] [int] NOT NULL,
-	[frequency] [int] NOT NULL,
-	[number_of_stop] [int] NOT NULL,
-	[number_of_bus] [int] NOT NULL,
-	[operating_hour_start] [time](7) NOT NULL,
-	[operating_hour_end] [time](7) NOT NULL,
-	[distance] [int] NOT NULL,
- CONSTRAINT [PK_Route] PRIMARY KEY CLUSTERED 
+
+CREATE TABLE [Bus].[BusTrip]
+(   
+      [trip_id] VARCHAR(50) NOT NULL,
+	  [bus_code] VARCHAR(50) NOT NULL,
+	  [date_id] VARCHAR(50) NOT NULL,
+	  [date] DATE NOT NULL,
+	  [depart_time] TIME(0) NOT NULL,
+	  [arrival_time] TIME(0) NOT NULL,
+	  [number_of_ticket] INT NOT NULL,
+	  [is_rush_hour] VARCHAR(50) NOT NULL,
+	  [modified_date] DATETIME,
+CONSTRAINT [PK_bustrip] PRIMARY KEY ([trip_id])
+);
+
+
+CREATE TABLE [Bus].[BusCalendar]
 (
-	[route_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+	[date_id] VARCHAR(50) NOT NULL,
+	[date] DATE NOT NULL,
+	[week_day] VARCHAR(50) NOT NULL,
+	[day] TINYINT NOT NULL,
+	[month] TINYINT NOT NULL,
+	[year] INT NOT NULL,
+CONSTRAINT [PK_buscalendar] PRIMARY KEY ([date_id])
+);
 
--- Schedule.StopRoute
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Schedule].[StopRoute](
-	[bus_stop_id] [int] NULL,
-	[route_id] [int] NULL,
-	[arrival_time] [time](7) NULL
-) ON [PRIMARY]
-GO
 
--- Schedule.StopTime
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Schedule].[StopTime](
-	[stop_time_id] [int] NOT NULL,
-	[trip_id] [int] NOT NULL,
-	[bus_stop_id] [int] NOT NULL,
- CONSTRAINT [PK_StopTime] PRIMARY KEY CLUSTERED 
+-- Insert calendar data into [BusCalendar]
+
+DECLARE @StartDate  date = '20210101';
+
+DECLARE @CutoffDate date = DATEADD(DAY, -1, DATEADD(YEAR, 2, @StartDate));
+
+;WITH seq(n) AS 
 (
-	[stop_time_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+  SELECT 0 UNION ALL SELECT n + 1 FROM seq
+  WHERE n < DATEDIFF(DAY, @StartDate, @CutoffDate)
+),
+d(d) AS 
+(
+  SELECT DATEADD(DAY, n, @StartDate) FROM seq
+),
+src AS
+(
+  SELECT
+    DateID = CONVERT(date, d),
+	TheDate = CONVERT(date, d),
+	TheDayName = CONVERT(VARCHAR(50), DATENAME(WEEKDAY, d)),
+    TheDay = CONVERT(TINYINT, DATEPART(DAY, d)),
+    TheMonth = CONVERT(TINYINT, DATEPART(MONTH, d)),
+    TheYear = CONVERT(INT, DATEPART(YEAR, d))
+  FROM d
+)
+INSERT INTO [Bus].[BusCalendar]([date_id], [date], [week_day], [day], [month], [year])
+SELECT FORMAT (DateID, 'yyyyMMdd') as DateID, TheDate, TheDayName, TheDay,
+TheMonth, TheYear FROM src
+  ORDER BY TheDate
+  OPTION (MAXRECURSION 0);
+
+
+-- Add constraints
+---BusRoute
+ALTER TABLE [Bus].[BusRoute]
+WITH CHECK
+ADD CONSTRAINT [FK_bustype] FOREIGN KEY ([bus_type_id])
+REFERENCES [Bus].[BusType]([bus_type_id]);
 GO
 
--- Schedule.Trip
+ALTER TABLE [Bus].[BusRoute] CHECK CONSTRAINT [FK_bustype];
+GO
+
+---BusInfo
+ALTER TABLE [Bus].[BusInfo]
+WITH CHECK
+ADD CONSTRAINT [FK_routeid] FOREIGN KEY ([route_id])
+REFERENCES [Bus].[BusRoute]([route_id]);
+GO
+
+ALTER TABLE [Bus].[BusInfo] CHECK CONSTRAINT [FK_routeid];
+GO
+
+---BusTrip
+ALTER TABLE [Bus].[BusTrip]
+WITH CHECK
+ADD CONSTRAINT [FK_buscode] FOREIGN KEY ([bus_code])
+REFERENCES [Bus].[BusInfo]([bus_code]);
+GO
+
+ALTER TABLE [Bus].[BusTrip] CHECK CONSTRAINT [FK_buscode];
+GO
+
+ALTER TABLE [Bus].[BusTrip]
+WITH CHECK
+ADD CONSTRAINT [FK_dateid] FOREIGN KEY ([date_id])
+REFERENCES [Bus].[BusCalendar]([date_id]);
+GO
+
+ALTER TABLE [Bus].[BusTrip] CHECK CONSTRAINT [FK_dateid];
+GO
+
+--Create Views
+GO
+CREATE VIEW [V_Dim_BusType] AS
+SELECT [bus_type_id], [bus_type], [modified_date]
+FROM [Bus].[BusType];
+GO
+CREATE VIEW [V_Dim_BusRoute] AS
+SELECT [route_id], [route_name], [depart_address],[frequency],
+       [operating_start_hour], [operating_end_hour], [modified_date]
+FROM [Bus].[BusRoute];
+
+GO
+CREATE VIEW [V_Dim_BusInfo] AS
+SELECT [bus_code],
+	[seat_capacity],
+	[max_capacity],
+	[modified_date]
+FROM [Bus].[BusInfo];
+
+GO
+CREATE VIEW [V_A] AS
+SELECT [Bus].[BusInfo].[bus_code],
+  [Bus].[BusInfo].[route_id],
+  [Bus].[BusRoute].[bus_type_id],
+  [Bus].[BusRoute].[number_of_busstop],
+  [Bus].[BusRoute].[standard_duration],
+  [Bus].[BusRoute].[route_distance],
+  B.[fare]
+FROM [Bus].[BusInfo]
+LEFT JOIN [Bus].[BusRoute] ON [Bus].[BusInfo].[route_id] = [Bus].[BusRoute].[route_id]
+LEFT JOIN ( SELECT [Bus].[BusRoute].[route_id], [Bus].[BusType].[fare]
+             FROM [Bus].[BusRoute]
+			 LEFT JOIN [Bus].[BusType] ON [Bus].[BusRoute].[bus_type_id] = [Bus].[BusType].[bus_type_id]
+		  ) B ON [Bus].[BusInfo].[route_id] = B.[route_id];
+
+GO
+CREATE VIEW [V_Fact_BusTrip] AS
+SELECT [Bus].[BusTrip].[trip_id] AS [trip_id],
+       [Bus].[BusTrip].[bus_code] AS [bus_code],
+	   [V_A].[route_id] AS [route_id],
+	   [V_A].[bus_type_id] AS [bus_type_id],
+	   [Bus].[BusCalendar].[date_id] AS [date_id],
+	   [Bus].[BusTrip].[date] AS [date],
+	   [Bus].[BusTrip].[depart_time] AS [depart_time],
+	   [Bus].[BusTrip].[arrival_time] AS [arrival_time],
+	   [Bus].[BusTrip].[is_rush_hour] AS [is_rush_hour],
+	   DATEDIFF(MINUTE, [depart_time], [arrival_time]) AS [real_duration],
+	   [V_A].[standard_duration] AS [standard_duration],
+(CASE WHEN DATEDIFF(MINUTE, [depart_time], [arrival_time]) <= [standard_duration] THEN 'ONTIME' ELSE 'LATE' END) AS [status],
+       [V_A].[number_of_busstop] AS [number_of_busstop],
+	   [V_A].[route_distance] AS [route_distance],
+	   ROUND(([route_distance]/(CAST((DATEDIFF(MINUTE, [depart_time], [arrival_time])) AS FLOAT)/60)),2) AS [average_velocity],
+	   [Bus].[BusTrip].[number_of_ticket] AS [number_of_ticket],
+	   [V_A].[fare] AS [fare],
+	   ([number_of_ticket]*[fare]) AS [revenue],
+	   [Bus].[BusTrip].[modified_date] AS [modified_date]
+FROM [Bus].[BusTrip]
+LEFT JOIN [V_A] ON [Bus].[BusTrip].[bus_code] = [V_A].[bus_code]
+LEFT JOIN [Bus].[BusCalendar] ON [Bus].[BusTrip].[date_id] = [Bus].[BusCalendar].[date_id];
+GO
+
+--Create A Log Table To Track Changes To Database Objects
+USE [PublicTransportation]
+GO
+ 
+SET ANSI_NULLS ON
+GO
+ 
+SET QUOTED_IDENTIFIER ON
+GO
+ 
+CREATE TABLE [ChangeLog]
+(
+    [log_id] [INT] IDENTITY(1,1) NOT NULL,
+    [database_name] VARCHAR(256) NOT NULL,
+    [event_type] VARCHAR(50) NOT NULL,
+    [object_name] VARCHAR(256) NOT NULL,
+    [object_type] VARCHAR(25) NOT NULL,
+    [sql_command] VARCHAR(MAX) NOT NULL,
+    [event_date] DATETIME NOT NULL,
+    [login_name] VARCHAR(256) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
+GO
+ 
+ALTER TABLE [ChangeLog]
+ADD CONSTRAINT [DF_EventsLog_EventDate] DEFAULT (GETDATE()) FOR [event_date];
+GO
+
+--Create triggers
+
+--- Create trigger to update [modified_date] for [Bus].[BusType] table
+USE [PublicTransportation]
+ GO
+ SET ANSI_NULLS ON
+ GO
+ SET QUOTED_IDENTIFIER ON
+ GO
+ CREATE TRIGGER [bustype_modified_date] on [Bus].[BusType] AFTER INSERT, UPDATE AS
+ BEGIN
+     UPDATE [Bus].[BusType]
+     SET [modified_date] = CURRENT_TIMESTAMP
+     FROM [Bus].[BusType] INNER JOIN inserted i ON [Bus].[BusType].[bus_type_id] = i.[bus_type_id]
+ END;
+ GO
+ ALTER TABLE [Bus].[BusType] ENABLE TRIGGER [bustype_modified_date];
+ GO
+
+ ---Create trigger to update [modified_date] for [Bus].[BusRoute] table
+ USE [PublicTransportation]
+ GO
+ SET ANSI_NULLS ON
+ GO
+ SET QUOTED_IDENTIFIER ON
+ GO
+ CREATE TRIGGER [busroute_modified_date] on [Bus].[BusRoute] AFTER INSERT, UPDATE AS
+ BEGIN
+     UPDATE [Bus].[BusRoute]
+     SET [modified_date] = CURRENT_TIMESTAMP
+     FROM [Bus].[BusRoute] INNER JOIN inserted i ON [Bus].[BusRoute].[route_id] = i.[route_id]
+ END;
+ GO
+ ALTER TABLE [Bus].[BusRoute] ENABLE TRIGGER [busroute_modified_date];
+ GO
+
+  ---Create trigger to update [modified_date] for [Bus].[BusInfo] table
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ CREATE TRIGGER [Bus].[businfo_modified_date] on [Bus].[BusInfo] AFTER INSERT, UPDATE AS
+ BEGIN
+     UPDATE [Bus].[BusInfo]
+     SET [modified_date] = CURRENT_TIMESTAMP
+     FROM [Bus].[BusInfo] INNER JOIN inserted i ON [Bus].[BusInfo].[bus_code] = i.[bus_code]
+ END;
+GO
+
+ALTER TABLE [Bus].[BusInfo] ENABLE TRIGGER [businfo_modified_date];
+GO
+
+ ---Create trigger to update [modified_date] for [Bus].[BusTrip] table
+ USE [PublicTransportation]
+ GO
+ SET ANSI_NULLS ON
+ GO
+ SET QUOTED_IDENTIFIER ON
+ GO
+ CREATE TRIGGER [bustrip_modified_date] on [Bus].[BusTrip] AFTER INSERT, UPDATE AS
+ BEGIN
+     UPDATE [Bus].[BusTrip]
+     SET [modified_date] = CURRENT_TIMESTAMP
+     FROM [Bus].[BusTrip] INNER JOIN inserted i ON [Bus].[BusTrip].[trip_id] = i.[trip_id]
+ END;
+ GO
+ ALTER TABLE [Bus].[BusTrip] ENABLE TRIGGER [bustrip_modified_date];
+ GO
+
+ ---Create trigger to backup objects with [dbo].[ChangeLog] table
+USE [PublicTransportation]
+GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [Schedule].[Trip](
-	[trip_id] [int] NOT NULL,
-	[bus_code] [int] NOT NULL,
-	[trip_headsign] [nvarchar](150) NOT NULL,
-    [date_stop] [date] NOT NULL,
-	[time_stop] [time](7) NOT NULL,
-	[number_of_ticket] [int] NOT NULL,
- CONSTRAINT [PK_Trip] PRIMARY KEY CLUSTERED 
-(
-	[trip_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+ 
+CREATE TRIGGER [backup_objects]
+ON DATABASE
+FOR CREATE_PROCEDURE, 
+    ALTER_PROCEDURE, 
+    DROP_PROCEDURE,
+    CREATE_TABLE, 
+    ALTER_TABLE, 
+    DROP_TABLE,
+    CREATE_FUNCTION, 
+    ALTER_FUNCTION, 
+    DROP_FUNCTION,
+    CREATE_VIEW,
+    ALTER_VIEW,
+    DROP_VIEW
+AS
+ 
+SET NOCOUNT ON
+ 
+DECLARE @data XML
+SET @data = EVENTDATA()
+ 
+INSERT INTO changelog(database_name, event_type, 
+    object_name, object_type, sql_command, login_name)
+VALUES(
+@data.value('(/EVENT_INSTANCE/DatabaseName)[1]', 'varchar(256)'),
+@data.value('(/EVENT_INSTANCE/EventType)[1]', 'varchar(50)'), 
+@data.value('(/EVENT_INSTANCE/ObjectName)[1]', 'varchar(256)'), 
+@data.value('(/EVENT_INSTANCE/ObjectType)[1]', 'varchar(25)'), 
+@data.value('(/EVENT_INSTANCE/TSQLCommand)[1]', 'varchar(max)'), 
+@data.value('(/EVENT_INSTANCE/LoginName)[1]', 'varchar(256)')
+);
 GO
-
--- Add primary and reference key
-ALTER TABLE [Agency].[Bus]  WITH CHECK ADD  CONSTRAINT [FK_Bus_Route] FOREIGN KEY([route_id])
-REFERENCES [Schedule].[Route] ([route_id])
+ 
+ENABLE TRIGGER [backup_objects] ON DATABASE;
 GO
-ALTER TABLE [Agency].[Bus] CHECK CONSTRAINT [FK_Bus_Route]
-GO
-ALTER TABLE [Schedule].[Route]  WITH CHECK ADD  CONSTRAINT [FK_Route_Agency] FOREIGN KEY([agency_id])
-REFERENCES [Agency].[Agency] ([agency_id])
-GO
-ALTER TABLE [Schedule].[Route] CHECK CONSTRAINT [FK_Route_Agency]
-GO
-ALTER TABLE [Schedule].[StopRoute]  WITH CHECK ADD  CONSTRAINT [FK_StopRoute_BusStop] FOREIGN KEY([bus_stop_id])
-REFERENCES [Schedule].[BusStop] ([bus_stop_id])
-GO
-ALTER TABLE [Schedule].[StopRoute] CHECK CONSTRAINT [FK_StopRoute_BusStop]
-GO
-ALTER TABLE [Schedule].[StopRoute]  WITH CHECK ADD  CONSTRAINT [FK_StopRoute_Route] FOREIGN KEY([route_id])
-REFERENCES [Schedule].[Route] ([route_id])
-GO
-ALTER TABLE [Schedule].[StopRoute] CHECK CONSTRAINT [FK_StopRoute_Route]
-GO
-ALTER TABLE [Schedule].[StopTime]  WITH CHECK ADD  CONSTRAINT [FK_StopTime_BusStop] FOREIGN KEY([bus_stop_id])
-REFERENCES [Schedule].[BusStop] ([bus_stop_id])
-GO
-ALTER TABLE [Schedule].[StopTime] CHECK CONSTRAINT [FK_StopTime_BusStop]
-GO
-ALTER TABLE [Schedule].[StopTime]  WITH CHECK ADD  CONSTRAINT [FK_StopTime_Trip] FOREIGN KEY([trip_id])
-REFERENCES [Schedule].[Trip] ([trip_id])
-GO
-ALTER TABLE [Schedule].[StopTime] CHECK CONSTRAINT [FK_StopTime_Trip]
-GO
-ALTER TABLE [Schedule].[Trip]  WITH CHECK ADD  CONSTRAINT [FK_Trip_Bus] FOREIGN KEY([bus_code])
-REFERENCES [Agency].[Bus] ([bus_code])
-GO
-ALTER TABLE [Schedule].[Trip] CHECK CONSTRAINT [FK_Trip_Bus]
-GO
-USE [master]
-GO
-ALTER DATABASE [PublicTransportation] SET  READ_WRITE 
-GO
-
--- Create Agent Job / Schedule
-
--- Create Stored Procedure
