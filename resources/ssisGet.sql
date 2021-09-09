22220d4145f675784cacb73d5ec3a40d9d24155f
huynguyen314 <billnguyen2091@gmail.com>
2021-09-09 11:03:35 +0700

    Update files related snowflake

--- resources/ssisGet.sql
+++ resources/ssisGet.sql
@@ -1,3 +1,6 @@
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE = TRANSPORT_WH;
+USE DATABASE PUBLICTRANSPORTATION;
 !set variable_substitution=true;
 !define path='C:/Users/&{name}/project/work-folder';
 get @unload_stage file://&path;
--- resources/ssisPut.sql
+++ resources/ssisPut.sql
@@ -1,3 +1,6 @@
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE = TRANSPORT_WH;
+USE DATABASE PUBLICTRANSPORTATION;
 !set variable_substitution=true;
 !define path='C:/Users/&{name}/project/buffer-folder/*.csv';
 put file://&path @upload_stage;
